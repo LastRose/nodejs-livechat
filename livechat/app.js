3dd3886a08a2a41d3097e731e5edd52b77f9a75b
Lastrose <github@lastrose.com>
2011-12-17 17:30:57 -0500

    latest version

--- livechat/app.js
+++ livechat/app.js
@@ -80,16 +80,38 @@ app.listen(8000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 var everyone = nowjs.initialize(app);
+var admins = new Array();
+
 
 everyone.now.joinGroup = function(groupname){
 	var group = nowjs.getGroup(groupname);
 	console.log('admin joined group '+ groupname);
 	group.addUser(this.user.clientId);
 	group.now.receiveMessage(this.now.name, 'Admin has joined the chat',groupname);
+	admins[admins.length] = this.user.clientId;
 }
 
 everyone.now.popGroups = function(){
 	nowjs.getGroups(function(groups){
+		var i = groups.length;
+		for(i; i>0;--i){
+			nowjs.getGroup(groups[i]).getUsers(function(users){
+				var x = admins.length;
+				for(x; x>0; --x){
+					ind = users.indexOf(admins[x]);
+					if(ind != -1){
+						users.splice(ind,1);
+					}
+				}
+				if(!users.length){
+					groups.splice(i,1);
+				}
+				undef=groups.indexOf('undefined');
+				if(undef != -1){
+					groups.splice(undef,1);
+				}
+			})
+		}
 		everyone.now.setGroups(groups);
 	});
 }
@@ -101,7 +123,12 @@ nowjs.on('connect', function(){
       console.log("Joined: " + this.now.name);
 			everyone.now.popGroups();
 });
-
+nowjs.on('disconnect', function(){
+			ind = admins.indexOf(this.user.clientId);
+			if(ind != -1){			
+				admins.splice(admins.ind,1);			
+			}
+})
 nowjs.on('newgroup',function(group){
 	  	console.log('You have successfully created the group `' + group.groupName + '`');
 })
@@ -109,3 +136,9 @@ everyone.now.distributeMessage = function(message,groupname){
 	group = nowjs.getGroup(groupname);
 	group.now.receiveMessage(this.now.name, message, groupname);
 }
+
+//on admin connect add userID to array
+//on list of groups
+//remove users in admin array from lost of users in that group
+//if the number of users in the group is 0, remove it from the list of groups.
+//on admin disconnect, remove userid from admin array.
